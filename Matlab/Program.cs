68bd7ea360c62a85857ea81c59980785b580f341
Gee <92035453+ManGeelato@users.noreply.github.com>
2024-11-12 12:28:30 +0200

    Add project files.

---
+++ Matlab/Program.cs
@@ -0,0 +1,27 @@
+﻿using System; 
+using System.Collections.Generic; 
+using System.Text; 
+
+namespace Matlab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MLApp.MLApp matlab = new MLApp.MLApp();
+
+            matlab.Execute(@"cd c:\temp\matlabFunction");
+
+            object result = null;
+
+            matlab.Feval("myfunc", 3, out result, 5, 50, "world oo");
+
+            object[] res = result as object[];
+
+            Console.WriteLine(res[0]);
+            Console.WriteLine(res[1]);
+            Console.WriteLine(res[2]);
+            Console.ReadLine();
+        }
+    }
+}
